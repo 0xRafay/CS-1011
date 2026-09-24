d150159551fbde8f46f9a1d4862d8c4431a36259
0xRafay <abdulrafay.src@gmail.com>
2026-09-24 08:17:09 -0400

    feat: complete fax assignment

--- src/fax.cpp
+++ src/fax.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 int main() {
     /* Suppose that the cost of sending an international fax is calculated as
      * follows:
@@ -7,5 +8,25 @@ int main() {
      * enter the number of pages to be faxed. The algorithm then
      * uses the number of pages to be faxed to calculate the amount due
      */
+    int noOfPages = 0;
+
+    std::cout << "Enter No. of pages: ";
+    std::cin >> noOfPages;
+
+    double totalBill = 0;
+
+    totalBill += 3.0; // adding 3$ as a onetime service fee
+
+    // We count pages, and pages are numbered 1, 2, 3…,
+    //  so i starts at 1
+    // and represents the current page number.
+    for (int i = 1; i <= noOfPages; i++) {
+        if (i <= 10) {
+            totalBill += 0.20; // per page for first 10 pages
+        } else {
+            totalBill += 0.10;
+        }
+    }
+    std::cout << "Total Bill: $" << totalBill << std::endl;
     return 0;
 }
