fcf4d26e9bb42ac0026f793e65f36c4a6c89bc84
0xRafay <abdulrafay.src@gmail.com>
2026-09-24 07:57:51 -0400

    feat: add fax assignment

---
+++ src/fax.cpp
@@ -0,0 +1,11 @@
+int main() {
+    /* Suppose that the cost of sending an international fax is calculated as
+     * follows:
+     * the service charge is $3.00, $0.20 per page for the first 10
+     * pages, and $0.10 for each additional page.
+     * Design an algorithm that asks the user to
+     * enter the number of pages to be faxed. The algorithm then
+     * uses the number of pages to be faxed to calculate the amount due
+     */
+    return 0;
+}
